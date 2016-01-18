132c6f260c8e8a40b2982415162bf06414c9016a
m4riusz <marr994@o2.pl>
2016-01-18 20:16:45 +0100

    small fix

--- include/Constants.h
+++ include/Constants.h
@@ -5,6 +5,6 @@ const unsigned short numberOfWalls = 6;
 const unsigned short numberOfCells = 9;
 const unsigned short numberOfCellsInRow = 3;
 const float drawingPercentShift = 0.17;
-const unsigned BLUR_DAGREE = 11;
+const unsigned BLUR_DEGREE = 5;
 
 #endif // CONSTANTS_H_INCLUDED
